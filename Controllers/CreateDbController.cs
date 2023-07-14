abd1a2c9bd239594936dc544504380d473184ec9
vicmen86 <victrinsumos@gmail.com>
2023-07-14 10:36:12 -0300

    Controllers y SErvicios

---
+++ Controllers/CreateDbController.cs
@@ -0,0 +1,12 @@
+using apiNet;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiNet.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class CreateDbController : ControllerBase
+{
+  
+}
